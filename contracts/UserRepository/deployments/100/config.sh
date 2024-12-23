f8438a627665a7a1a77c4a154a3329c59ee7a53e
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2024-12-23 01:33:16 +0000

    add gnosis deployment configuration

---
+++ contracts/UserRepository/deployments/100/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.28"
+)
+evm_version=(
+  ["1.0"]="cancun"
+)
+contract_address=(
+  ["1.0"]="0x7EBb73c91A04E0aB7a0cDe4e43f9b09eE3464c36"
+)
