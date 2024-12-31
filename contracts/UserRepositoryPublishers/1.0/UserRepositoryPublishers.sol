33093075bacf0a515d0bda1e9777f90f5ccc283a
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2024-12-31 13:44:12 +0000

    add user repository publishers contract and deployment configuration

---
+++ contracts/UserRepositoryPublishers/1.0/UserRepositoryPublishers.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title User Repository Publishers
+ * @dev User Repository Publishers.
+ */
+contract UserRepositoryPublishers {
+
+    address public immutable deployer = 0xea02F564664A477286B93712829180be4764fAe2;
+    // dvorak is trustedEnroller
+    address public immutable trustedEnroller = 0x87003Bd6C074C713783df04f36517451fF34CBEf;
+   
+    string public hijess = "lovewilltriumph";
+    string public version = "1.0";
+    uint256 public unit = 1000000000000000000;
+    uint256 public enrollingFee = 10 * unit;
+
+    uint256 public publisherNo = 1; 
+    mapping(uint256 => address) public publisher;
+    mapping(uint256 => address) public enroller;
+    mapping(address => uint256) public listed;
+    constructor() {}
+
+    /**
+     * @dev List a packager.
+     * @param _publisher User Repository Publisher sender is listing.
+     */
+    function listPackager(
+      address _publisher)
+      public
+      payable {
+      if ( msg.sender != trustedEnroller ) {
+        require(
+          listed[_publisher] == 0,
+          "the publisher has already been listed");
+        require(
+          msg.value >= enrollingFee,
+          "tried to list the publisher for less than the enrolling fee.");
+        payable(
+	  deployer).transfer(
+            msg.value);
+      }
+      publisher[publisherNo] = _publisher;
+      enroller[publisherNo] = msg.sender;
+      publisherNo = publisherNo + 1;
+    }
+
+    /**
+     * @dev Read User Repository publisher ID.
+     * @param _publisher User Repository publisher.
+     */
+    function readPublisherNo(
+      address _publisher)
+    public
+    view
+    returns (uint256)
+    {
+      return listed[_publisher];
+    }
+
+    /**
+     * @dev Read User Repository publisher enroller.
+     * @param _publisherNo User Repository publisher ID.
+     */
+    function readEnrollerNo(
+      uint256 _publisherNo)
+    public
+    view
+    returns (address)
+    {
+      return enroller[_publisherNo];
+    }
+
+    /**
+     * @dev Read User Repository publisher enroller.
+     * @param _publisher User Repository publisher.
+     */
+    function readEnroller(
+      address _publisher)
+    public
+    view
+    returns (address)
+    {
+      return enroller[listed[_publisher]];
+    }
+
+}
---
+++ contracts/UserRepositoryPublishers/deployments/100/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.28"
+)
+evm_version=(
+  ["1.0"]="cancun"
+)
+contract_address=(
+  ["1.0"]="0x136BB1FBCF39AB6A890E0681c7f5bAc28c72A8A4"
+)
