3e76c3f38db40ea4284b3d5c7ed8ef1d27e06312
Truocolo <truocolo@0x6E5163fC4BFc1511Dbe06bB605cc14a3e462332b>
2025-01-22 21:38:24 +0100

    add deployment config

---
+++ contracts/PackagePublishers/deployments/100/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.28"
+)
+evm_version=(
+  ["1.0"]="cancun"
+)
+contract_address=(
+  ["1.0"]="0x607EcD098f574E0904C4a53B8a620246b4B6FDfe"
+)
