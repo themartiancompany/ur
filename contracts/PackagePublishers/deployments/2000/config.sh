e75096617a4da6285be91acdf8f9f161b4686c44
Truocolo (kf) <truocolo@0x6E5163fC4BFc1511Dbe06bB605cc14a3e462332b>
2025-02-27 15:31:53 +0100

    come on lets make you become ur doge-baked developers

---
+++ contracts/PackagePublishers/deployments/2000/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.24"
+)
+evm_version=(
+  ["1.0"]="paris"
+)
+contract_address=(
+  ["1.0"]="0x389182e900E76aE20719025F1DB9aA989E9dFf13"
+)
