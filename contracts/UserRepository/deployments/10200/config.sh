ec348e0294c0581c3ae604e2d7e66db8800bf5ee
Truocolo (kf) <truocolo@0x6E5163fC4BFc1511Dbe06bB605cc14a3e462332b>
2025-04-08 16:34:01 +0200

    im not goin slowly, im not chillin, im just manxplainin

---
+++ contracts/UserRepository/deployments/10200/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.1"]="0.8.28"
+)
+evm_version=(
+  ["1.1"]="cancun"
+)
+contract_address=(
+  ["1.1"]="0xA321bAE0ec4F58abeC075957683eAc92f989a9D8"
+)
