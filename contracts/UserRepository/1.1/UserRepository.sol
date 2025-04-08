f026aa8b7265dc76b63c637c5a27f9d71417c13b
Truocolo (kf) <truocolo@0x6E5163fC4BFc1511Dbe06bB605cc14a3e462332b>
2025-04-08 15:48:53 +0200

    they dont know its still you know kinda romantic even im apparently alone right now as the camera is kinda you know. theres like a clossroom downstairs staring at me ahahahha

--- contracts/UserRepository/1.1/UserRepository.sol
+++ contracts/UserRepository/1.1/UserRepository.sol
@@ -55,7 +55,6 @@ contract UserRepository {
     uint256 public baseRevenueThreshold = unit;
     uint256 public fullRevenueThreshold = 10 * unit;
     uint256 public scale = 1000000000;
-    address public immutable nullAddress 0x0;
 
     mapping(
       address => uint256 ) public packageNo;
@@ -351,10 +350,10 @@ contract UserRepository {
             _package][
             _publisher][
               _revision];
-        if ( _currency == nullAddress ) {
+        if ( _currency == address(0) ) {
           uint256 _value =
             msg.value;
-        else if ( _currency != nullAddress ) {
+        else if ( _currency != address(0) ) {
           uint256 _value =
             _amount;
         }
@@ -365,7 +364,7 @@ contract UserRepository {
         uint256 _publisherShare =
             getPublisherShare(
               _price);
-        if ( _currency == nullAddress ) {
+        if ( _currency == address(0) ) {
           payable(
             _publisher).transfer(
               _publisherShare);
@@ -373,7 +372,7 @@ contract UserRepository {
 	      deployer).transfer(
                 _value - _publisherShare);
 	  }
-        else if ( _currency != nullAddress ) {
+        else if ( _currency != address(0) ) {
           IERC20 _token = IERC20(
             _currency);
           require(
